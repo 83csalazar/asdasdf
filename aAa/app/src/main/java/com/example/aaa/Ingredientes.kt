9f1dac3c6a777e5f3ca437f3d164a4691e26e3e2
83csalazar <135677850+83csalazar@users.noreply.github.com>
2023-12-08 18:55:25 -0300

    primer guardado trabajo final android login listo home en proceso (listo boton facturas ) facturas listas vista y visor en pantalla ingredientes falta el proceso y las vistas

---
+++ aAa/app/src/main/java/com/example/aaa/Ingredientes.kt
@@ -0,0 +1,11 @@
+package com.example.aaa
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class Ingredientes : AppCompatActivity() {
+
+}
